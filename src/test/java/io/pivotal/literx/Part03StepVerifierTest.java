3df664f2c940a8430a8d8340413f72bc2f8eea80
vcuca <vcuca@execom.eu>
2017-11-07 22:34:46 +0100

    Completed Flux, Mono, StepVerifier and Transformation steps.

--- src/test/java/io/pivotal/literx/Part03StepVerifierTest.java
+++ src/test/java/io/pivotal/literx/Part03StepVerifierTest.java
@@ -19,6 +19,7 @@ package io.pivotal.literx;
 import java.time.Duration;
 
 import io.pivotal.literx.domain.User;
+import org.junit.Assert;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -33,39 +34,38 @@ public class Part03StepVerifierTest {
 
 	Part03StepVerifier workshop = new Part03StepVerifier();
 
-//========================================================================================
+	//========================================================================================
 
 	@Test
 	public void expectElementsThenComplete() {
 		workshop.expectFooBarComplete(Flux.just("foo", "bar"));
 	}
 
-//========================================================================================
+	//========================================================================================
 
 	@Test
 	public void expect2ElementsThenError() {
 		workshop.expectFooBarError(Flux.just("foo", "bar").concatWith(Mono.error(new RuntimeException())));
 	}
 
-//========================================================================================
+	//========================================================================================
 
 	@Test
 	public void expectElementsWithThenComplete() {
 		workshop.expectSkylerJesseComplete(Flux.just(new User("swhite", null, null), new User("jpinkman", null, null)));
 	}
 
-//========================================================================================
+	//========================================================================================
 
 	@Test
 	public void count() {
 		workshop.expect10Elements(Flux.interval(Duration.ofSeconds(1)).take(10));
 	}
 
-//========================================================================================
+	//========================================================================================
 
 	@Test
 	public void countWithVirtualTime() {
 		workshop.expect3600Elements(() -> Flux.interval(Duration.ofSeconds(1)).take(3600));
 	}
-
 }
